600337886ce96cb2fa929ccaf2775980ecfd5b80
zzs <596885319@qq.com>
2018-10-11 15:59:13 +0800

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
     public function show(User $user)
     {
         return view('users.show',compact('user'));//创建一个包含变量名和它们的值的数组
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
