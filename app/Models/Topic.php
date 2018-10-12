a88b6486c1c9855f59fa0a5527516209af187117
zzs <596885319@qq.com>
2018-10-12 11:59:56 +0800

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -1,8 +1,17 @@
 <?php
 
 namespace App\Models;
+use App\Models\Category;
+use App\Models\User;
 
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
