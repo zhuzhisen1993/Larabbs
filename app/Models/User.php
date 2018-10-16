65e0df0ed0751f95e101a1b2e06a7723ba8028f8
zzs <596885319@qq.com>
2018-10-16 09:52:06 +0800

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     public function topics(){
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model){
+        return $this->id == $model->user_id;
+    }
+    
 }
