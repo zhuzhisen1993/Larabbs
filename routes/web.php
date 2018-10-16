4f5a7cb4ea70528d0df838a7f76c9ab387f4097c
zzs <596885319@qq.com>
2018-10-16 17:52:31 +0800

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //Route::put('users/edit','UsersController@edit')->name('users.edit');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
