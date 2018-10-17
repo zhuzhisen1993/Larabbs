def96d1c969b95d859c1807436517880cd43cfc4
zzs <596885319@qq.com>
2018-10-17 17:48:17 +0800

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,5 +35,9 @@ class User extends Authenticatable
     public function isAuthorOf($model){
         return $this->id == $model->user_id;
     }
-    
+
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
+
 }
---
+++ resources/views/topics/_reply_box.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/17
+ * Time: 17:38
+ */
